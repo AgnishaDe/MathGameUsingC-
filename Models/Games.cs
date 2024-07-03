3eb7cb27e3a247019c82b19ae23736c7845766c6
Agnisha De <t-agnishade@microsoft.com>
2024-07-03 21:33:08 +0530

    initial commit

---
+++ Models/Games.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__MathGameProject.Models
+{
+    internal class Games
+    {
+        //private int _score;
+
+        //public int Score 
+        //{ 
+        //    get { return _score; } 
+        //    set { _score = value; }
+        //}
+        internal DateTime Date { get; set; }
+
+        internal int Score { get; set; }
+
+        internal string Type { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using C__MathGameProject;
+
+var menu = new Menu();        //object of menu class
+
+var date = DateTime.UtcNow;
+
+string name = GetName();
+
+menu.showMenu(name,date);
+
+string GetName()
+{
+    Console.WriteLine("Please type your name");
+    var name = Console.ReadLine();
+    return name;
+}
+
+
